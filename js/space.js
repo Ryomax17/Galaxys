1526739787ed70cb582ce556c25984090902e30d
Ryomax17 <42577059+Ryomax17@users.noreply.github.com>
2023-09-06 21:11:12 -0300

    adsdadsd

--- js/space.js
+++ js/space.js
@@ -9,15 +9,24 @@ boton.addEventListener("click", function(){
 
 function search(){
 
-let busqueda = document.getElementById("inputBuscar");
+let busqueda = document.getElementById("inputBuscar").value;
 
 let response = fetch(`https://images-api.nasa.gov/search?q=${busqueda}`).then(response => response.json()) .then(data => {
-    const array = data;
-    console.table(array);
-    const url = data.collection.items[0].url
-    console.table(url);
+    const array = data.collection.items;
+    console.log(array);
 
-
+    array.forEach(element => {
+        
+        const tittle = element.tittle;
+        const description = element.description;
+        const link = element.links[0].href;
+        contenedor.innerHTML = `
+        <h2>${tittle}</h2>
+        <h4>Descripcion: ${description}</h4>
+        <img src="${link}" alt="Imagen de ${tittle}">
+    `;
+    }); {
+    }
 
 } );
 
