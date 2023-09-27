faabd7d86e87aa604b1deca2aee93720d98d0aa5
maximiliano836 <maxi10.02.00.g@gmail.com>
2023-09-27 19:06:03 -0300

    -

--- js/space.js
+++ js/space.js
@@ -1,47 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
-let boton = document.getElementById("btnBuscar");
+document.addEventListener("DOMContentLoaded", function () {
+  const boton = document.getElementById("btnBuscar");
+  const inputBuscar = document.getElementById("inputBuscar");
+  const contenedor = document.getElementById("contenedor");
 
-boton.addEventListener("click", function(){
-    
-    search();
-    
-    
-});
+  boton.addEventListener("click", function () {
+    const busqueda = inputBuscar.value.trim().toLowerCase();
+    contenedor.innerHTML = ""; 
 
-function search(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
 
-    let busqueda = document.getElementById("inputBuscar").value;
+        const matchingUsers = data.filter((data) =>
+        data.name.toLowerCase().includes(busqueda)
+        );
 
-    let response = fetch(`https://images-api.nasa.gov/search?q=${busqueda}`).then(response => response.json()) .then(data => {
-        const array = data.collection.items;
-        contenedor.innerHTML = "";
-        console.log(array);
+        if (matchingUsers.length === 0) {
+          contenedor.innerHTML = "No se encontraron usuarios.";
+        } else {
+          
+          matchingUsers.forEach((data) => {
+            const name = data.name;
+            const usuario = data.username;
+            const email = data.email;
 
-    array.forEach(element => {
-        
-        const tittle = element.data.length>0? element.data[0].title : "";
-        const description = element.data.length>0? element.data[0].description : "";
-        const link = element.links.length>0? element.links[0].href : "";
-        const date = element.data.length>0? element.data[0].date_created : "";
-
-        contenedor.innerHTML += `
-        <div class="galaxys">
-        <div class"imgs-container">
-        <img class="imgs" src="${link}" alt="Imagen de ${tittle}">
-        </div>
-        <div class="info">
-        <h4 class="title">${tittle}</h4>
-        <p class="description">Descripcion: ${description}</p>
-        <p class="date">${date}</p>
-        </div>
-        </div>
-    `;
-
-    }); 
-    
-
-});
-
-};
-
+            contenedor.innerHTML += `
+              <h2>${name}</h2>
+              <p>Usuario: ${usuario}</p>
+              <p>Email: ${email}</p>
+            `;
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Error al realizar la búsqueda:", error);
+      });
+  });
 });
